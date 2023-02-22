6127ac2338c1441d553b447f145633505fac9159
drewlandoch <drew.landoch@gmail.com>
2023-02-22 18:01:23 +0000

    status code lab completed

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -16,8 +16,7 @@ public class JavalinSingleton {
         
 
         app.get("/client-side-err", ctx -> {
-        
-            //write logic here
+                ctx.status(400);
             
         });
 
